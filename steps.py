34a3df37405deadb777ffcfbece1da8ed1c83a9e
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-10-03 23:16:27 -0500

    Defined function that takes in 3 params

--- steps.py
+++ steps.py
@@ -1,1 +1,8 @@
 print("Staircase2!")
+
+
+# Function will take in three parameters
+# steps: amount of steps staircase has,
+# max: maximum amount of steps that can be taken at one time,
+# array: we will dynamically store our answer in this array as we move forward
+def staircase_2(steps, max, array):
