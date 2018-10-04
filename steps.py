35cbee582fe59c27133695ca96dabde95875617e
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-10-03 23:17:35 -0500

    When steps equals 0-1, only one way to reach top.

--- steps.py
+++ steps.py
@@ -6,3 +6,6 @@ print("Staircase2!")
 # max: maximum amount of steps that can be taken at one time,
 # array: we will dynamically store our answer in this array as we move forward
 def staircase_2(steps, max, array):
+
+	# When steps equals 0-1, only one way to reach top.
+	if (steps <= 1): return 1
